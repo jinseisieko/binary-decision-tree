31ea62ef8043bc7f221cd7f88c7d9fe8daa1d046
jinseisieko <jinseisieko@gmail.com>
2025-11-29 15:15:50 +0300

    feat(core): renaming
    change functionality of library (remove complete)

---
+++ src/main/java/io/github/jinseisieko/bdtree/BDTreeNode.java
@@ -0,0 +1,4 @@
+package io.github.jinseisieko.bdtree;
+
+public abstract class BDTreeNode {
+}
--- src/test/java/io/github/jinseisieko/bdtree/BDTreeNodeTest.java
+++ src/test/java/io/github/jinseisieko/bdtree/BDTreeNodeTest.java
@@ -1,9 +1,9 @@
-package io.github.jinseisieko.cbdtree;
+package io.github.jinseisieko.bdtree;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 
-class CBDTreeNodeTest {
+class BDTreeNodeTest {
     @Test
     void test() {
         assertTrue(1 == 1);
