dde80dbf8cffa210e66c160d299ebda65f166874
jinseisieko <jinseisieko@gmail.com>
2025-12-02 11:51:10 +0300

    feat: add getters and setters for nodes

--- src/main/java/io/github/jinseisieko/bindecistree/DynamicBinDecisTreeNode.java
+++ src/main/java/io/github/jinseisieko/bindecistree/DynamicBinDecisTreeNode.java
@@ -11,4 +11,25 @@ abstract class DynamicBinDecisTreeNode<D, V> {
     public DynamicBinDecisTreeBranch<D,V> toBranch() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    public void setTrueNode(DynamicBinDecisTreeNode<D, V> trueNode) {
+        this.trueNode = trueNode;
+    }
+
+    public void setFalseNode(DynamicBinDecisTreeNode<D, V> falseNode) {
+        this.falseNode = falseNode;
+    }
+
+    public DynamicBinDecisTreeNode<D, V> getTrueNode() {
+        return trueNode;
+    }
+
+    public DynamicBinDecisTreeNode<D, V> getFalseNode() {
+        return falseNode;
+    }
+    
+    public void setAllNodes(DynamicBinDecisTreeNode<D, V> trueNode, DynamicBinDecisTreeNode<D, V> falseNode) {
+        setTrueNode(trueNode);
+        setFalseNode(falseNode);
+    }
 }
