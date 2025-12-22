dec971c4a4c870bac5c32a1c0f59d01e4d535e52
jinseisieko <jinseisieko@gmail.com>
2025-12-22 19:47:17 +0300

    docs(javadoc): write BinaryDecisionTree class description

--- src/main/java/io/github/jinseisieko/decisiontree/BinaryDecisionTree.java
+++ src/main/java/io/github/jinseisieko/decisiontree/BinaryDecisionTree.java
@@ -2,6 +2,20 @@ package io.github.jinseisieko.decisiontree;
 
 import java.util.List;
 
+/**
+ * Represents a binary decision tree that determines an output value of type {@code O}
+ * from a given input value of type {@code I}.
+ * 
+ * <p>Use the {@code DecisionTreeBuilder} instance returned by the {@code builder()} method to construct and modify a tree.
+ *
+ * <p>Each instance defines branching logic and corresponding leaf decisions that produce
+ * outcomes based on the evaluated input.
+ *
+ * @param <I> the type of the input value evaluated by the tree
+ * @param <O> the type of the output value produced by the tree
+ *
+ * @author jinseisieko
+ */
 public interface BinaryDecisionTree<I, O> {
 
     O decide(I input);
