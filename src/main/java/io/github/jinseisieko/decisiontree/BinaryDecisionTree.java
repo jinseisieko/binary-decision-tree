f52e0350ba44891cdfb1464d741c47c6eff93422
jinseisieko <jinseisieko@gmail.com>
2025-12-23 20:50:57 +0300

    docs(javaDoc): edit BinaryDecisionTree class javaDoc and write javaDoc for decide method

--- src/main/java/io/github/jinseisieko/decisiontree/BinaryDecisionTree.java
+++ src/main/java/io/github/jinseisieko/decisiontree/BinaryDecisionTree.java
@@ -6,18 +6,26 @@ import java.util.List;
  * Represents a binary decision tree that determines an output value of type {@code O}
  * from a given input value of type {@code I}.
  * 
- * <p>Use the {@code DecisionTreeBuilder} instance returned by the {@code builder()} method to construct and modify a tree.
+ * <p>Use the {@code DecisionTreeBuilder} instance returned by the {@code builder()}
+ *  method to construct and modify a tree.
  *
  * <p>Each instance defines branching logic and corresponding leaf decisions that produce
- * outcomes based on the evaluated input.
+ * outcomes based on the input value and conditions in internal nodes.
  *
- * @param <I> the type of the input value evaluated by the tree
- * @param <O> the type of the output value produced by the tree
+ * @param <I> the type of the input value
+ * @param <O> the type of the output value produced by the tree decision
  *
  * @author jinseisieko
  */
 public interface BinaryDecisionTree<I, O> {
 
+    /**
+     * Decides and returns value of type {@code O} based on input value of type {@code I}
+     * and conditions in tree.
+     * 
+     * @param input the input value of type {@code I}.
+     * @return the output value determined by the tree.
+     */
     O decide(I input);
 
     boolean isFullyDefined();
