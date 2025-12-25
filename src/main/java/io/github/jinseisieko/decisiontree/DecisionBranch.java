f43cbab18e13265470b11a6518f516f2091dc974
jinseisieko <jinseisieko@gmail.com>
2025-12-25 19:01:39 +0300

    docs(javaDoc): write DecisionBranch javaDoc

--- src/main/java/io/github/jinseisieko/decisiontree/DecisionBranch.java
+++ src/main/java/io/github/jinseisieko/decisiontree/DecisionBranch.java
@@ -1,5 +1,16 @@
 package io.github.jinseisieko.decisiontree;
 
+/**
+ * Represents a subtree rooted at some vertex within an existing binary decision tree.
+ * 
+ * <p>This interface provides a reference to a specific section of the tree rather than
+ * storing independent data.
+ * 
+ * @param <I> the type of the input value
+ * @param <O> the type of the output value
+ * 
+ * @author jinseisieko
+ */
 public interface DecisionBranch<I, O> {
 
     DecisionBranch<I, O> getTrueBranch();
